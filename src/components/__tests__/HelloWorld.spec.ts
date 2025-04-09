53ee65f56f099c8e11465c8e58f81a2a631d1878
Xuan <98876600+XuanCbbLin@users.noreply.github.com>
2025-04-09 13:53:13 +0800

    add catch dependencies

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -5,7 +5,7 @@ import HelloWorld from '../HelloWorld.vue'
 
 describe('HelloWorld', () => {
   it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
+    const wrapper = mount(HelloWorld, { props: { msg: 'Hello2 Vitest' } })
     expect(wrapper.text()).toContain('Hello2 Vitest')
   })
 })
